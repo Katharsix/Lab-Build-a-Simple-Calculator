e2d1c7c9f49ae8aabc7c97b1e5b6992ff460ef7d
Katharsix <diegofer0811@gmail.com>
2025-11-13 16:32:14 -0500

    Calculadora

---
+++ main.py
@@ -0,0 +1,8 @@
+#Seccion de insertar numeros
+#print ("CALCULADORA :D")
+#numero1 = float(input("Ingresa el primer digito:"))
+#numero2 = float(input("Ingresa el segundo digito:"))
+#Resultado
+#resultado = numero1 + numero2
+#print ("la suma es", resultado)
+#==============================================================
